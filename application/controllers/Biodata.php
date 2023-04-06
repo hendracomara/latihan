58c11d869445119f226d73c71520d8d65d16f45c
hendracomara <hendracomara1306@gmail.com>
2023-04-06 18:52:07 +0700

    halo

--- application/controllers/Biodata.php
+++ application/controllers/Biodata.php
@@ -1,10 +1,18 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Biodata extends CI_Controller {
 
-    public function biodataku()
+public function index()
     {
         $this->load->view('biodata');
+    }
+
+    public function penjumlahan($nilai1,$nilai2)
+    {
+        $data['nilai1'] = $nilai1;
+        $data['nilai2'] = $nilai2;
+        $data['hasil'] = $this->Model_aritmatika->penjumlahan($nilai1, $nilai2);
+
+        $this->load->view('View-latihan.php',$data);
     }
 }
---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section> 
+    <h1><?php echo $judul ?></h1> 
+ 
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Hendra</li> 
+        <li>Nama Belakang : Comara</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan pelita 10</li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>jakarta</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>bola</li> 
+    </ul> 
+</section>
